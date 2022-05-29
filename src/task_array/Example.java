bd9a91ab3dfa0ce7df53df754dd0f60f5ce68d89
Sergey <spapoyan1987@gmail.com>
2022-05-29 22:28:15 +0400

    Commit

---
+++ src/task_array/Example.java
@@ -0,0 +1,12 @@
+package task_array;
+
+public class Example {
+    public static void main(String[] args) {
+        int i = 100;
+        int j = 200;
+
+        while (++i<--j);{
+            System.out.println(i);
+        }
+    }
+}
--- src/task_array/OddArrayReverse.java
+++ src/task_array/OddArrayReverse.java
@@ -4,13 +4,18 @@ public class OddArrayReverse {
     public static void main(String[] args) {
 
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] reverseArray = new int[array.length];
 
         for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
             array[i] = array[array.length - 1 - i];
             array[array.length - 1 - i] = temp;
         }
-        for (int x : array) {
+
+        for (int i = 0; i < reverseArray.length; i++) {
+            reverseArray[i] = array[i];
+        }
+        for (int x : reverseArray) {
             System.out.print(x + " ");
         }
     }
